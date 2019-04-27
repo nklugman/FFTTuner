c2ec59f924805086da2e38dc025fe8dc400baed8
nklugman <nklugman@umich.edu>
2019-04-26 18:16:23 -0700

    Make it display some FFT data.

--- app/src/main/java/edu/berkeley/gridwatch/ffttuner/MainActivity.java
+++ app/src/main/java/edu/berkeley/gridwatch/ffttuner/MainActivity.java
@@ -2,11 +2,13 @@ package edu.berkeley.gridwatch.ffttuner;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +17,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
@@ -29,7 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
     private Button startbtn;
     private TextView state_txt;
-    private TextView array_txt;
+
+    private TextView display_txt_view;
+
+    String display_txt_str = "-1";
+
+    int UI_update_rate_ms = 2000;
+
     private RxPermissions mPermissions;
     //private StreamAudioRecorder mStreamAudioRecorder;
     //private StreamAudioPlayer mStreamAudioPlayer;
@@ -52,15 +65,30 @@ public class MainActivity extends AppCompatActivity {
         return mPermissions;
     }
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        //Handle UI
         startbtn = (Button) findViewById(R.id.startbtn);
         state_txt = (TextView) findViewById(R.id.state_text);
-        array_txt = (TextView) findViewById(R.id.array_text);
+        display_txt_view = (TextView) findViewById(R.id.display_text);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("display", display_txt_str);
+                display_txt_view.setText(display_txt_str);
+                handler.postDelayed(this, UI_update_rate_ms);
+            }
+        }, UI_update_rate_ms);
 
+
+        //Handle Permissions
         boolean isPermissionsGranted = getRxPermissions().isGranted(RECORD_AUDIO);
         if (!isPermissionsGranted) {
             state_txt.setText("press start to get permissions");
@@ -76,9 +104,9 @@ public class MainActivity extends AppCompatActivity {
                     mIsRecording = false;
                     state_txt.setText("stopped");
                     try {
-                        audioThread.stop();
+                        audioThread.interrupt();
                     } catch (Exception e) {
-                        Log.e("error", e.getMessage());
+                        Log.e("error", "stopping thread: " + e.getLocalizedMessage());
                     }
                 } else {
                     boolean isPermissionsGranted = getRxPermissions().isGranted(RECORD_AUDIO);
@@ -96,8 +124,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 }, Throwable::printStackTrace);
                     } else {
-                        //startRecord();
-                        doFFT();
+                        doFFT(); //MAIN CALL
                         startbtn.setText("Stop");
                         mIsRecording = true;
                         state_txt.setText("running");
@@ -110,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doFFT() {
+
         try {
             dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(sampleRate, fftsize, overlap);
 
@@ -123,6 +151,8 @@ public class MainActivity extends AppCompatActivity {
                     // TODO Auto-generated method stub
                 }
 
+
+
                 @Override
                 public boolean process(AudioEvent audioEvent) {
                     float[] audioFloatBuffer = audioEvent.getFloatBuffer();
@@ -131,9 +161,38 @@ public class MainActivity extends AppCompatActivity {
                     fft.forwardTransform(transformbuffer);
                     fft.modulus(transformbuffer, amplitudes);
 
+                    //Log.i("amp", Arrays.toString(amplitudes));
 
-                    Log.e("amp", amplitudes.toString());
-                    Log.e("fft", fft.toString());
+
+                    display_txt_str = "";
+
+                    for(int i = 0; i < audioFloatBuffer.length; i++){
+                        float hz = (float) fft.binToHz(i, sampleRate);
+                        try {
+                            if (i <= amplitudes.length) {
+                                float mag = amplitudes[i];
+
+                                if (hz >= 54 && hz <= 66 ||
+                                        hz >= 114 && hz <= 126 ||
+                                        hz >= 174 && hz <= 186 ||
+                                        hz >= 234 && hz <= 246 ||
+                                        hz >= 294 && hz <= 306) {
+                                    //Log.i("fft", String.valueOf(hz) + "," + String.valueOf(mag));
+                                    display_txt_str = display_txt_str + String.valueOf(hz) + "\t\t" + String.valueOf(mag) + "\n";
+                                }
+                            } else { //*****TODO**** This happens a lot... index into amplitudes might be wrong...
+                                //Log.e("error", "i:" + String.valueOf(i) + " " + "length:" + String.valueOf(amplitudes.length));
+                            }
+                        }
+                        catch (Exception e) {
+                            Log.e("error", "in process, " + e.getLocalizedMessage());
+                        }
+
+                    }
+
+                    //Log.e("amp", amplitudes.toString());
+                    //Log.e("fft", fft.toString());
+
 
                     return true;
                 }
